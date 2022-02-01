26cde99921cf1cb41e1ad7eb520b6ca52441fb4d
unknown <bidaybek96@mail.ru>
2022-02-01 18:38:34 +0600

    lab2

---
+++ __pycache__/turtle.py
@@ -0,0 +1,16 @@
+import turtle
+
+turtle.shape('turtle')
+turtle.forward(200)
+turtle.setheading(90)
+
+turtle.shape('turtle')
+turtle.forward(200)
+turtle.setheading(180)
+
+turtle.shape('turtle')
+turtle.forward(200)
+turtle.setheading(270)
+
+turtle.shape('turtle')
+turtle.forward(200)
