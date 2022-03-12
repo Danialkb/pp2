1481b42228e257b656ef50ef30c8307f18aa7f98
unknown <bidaybek96@mail.ru>
2022-03-12 16:12:37 +0600

    dk

--- lab5/RegEX/4.py
+++ lab5/RegEX/4.py
@@ -2,9 +2,11 @@ import re
 
 a = input()
 
-pattern = r'.*(?P<naiti>[A-Z][a-z]+).*'
+pattern = r'(?P<naiti>[A-Z][a-z]+)'
 
-print(re.search(pattern, a).group('naiti')) if re.search(pattern, a) != None else print('Ne naideno')
+print(re.findall(pattern, a))
+# print(re.search(pattern, a).group('naiti')) if re.search(pattern, a) != None else print('Ne naideno')
 
-#akakAkk -------- Akk
-#a888Alsoa; ------ Alsoa
+#akakAkk -------- ['Akk']
+#a888Alsoa; ------ ['Alsoa']
+# AgKdIe ---- ['Ag', 'Kd', 'Ie']
