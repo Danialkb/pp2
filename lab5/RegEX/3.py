4c9fa4649eb35dc61e1289982f1d0e425e9191fd
unknown <bidaybek96@mail.ru>
2022-03-12 13:16:07 +0600

    new

--- lab5/RegEX/3.py
+++ lab5/RegEX/3.py
@@ -1,7 +1,7 @@
 import re
 a = input()
 
-pattern = r'.*(?P<naiti>[a-z]+_[a-z]+).*'
+pattern = r'\W*\d*[A-Z]*(?P<naiti>[a-z]+_[a-z]+)\W*\d*[A-Z]*'
 
 print(re.search(pattern, a).group('naiti')) if re.search(pattern, a) != None else print('Ne naideno')
 #aa_sksks ----- aa_sksks
